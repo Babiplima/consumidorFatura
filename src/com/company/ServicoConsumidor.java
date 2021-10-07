ce90339b3e46322610a8172ce37409bf3be5473b
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-07 17:13:41 -0300

    Criar classe Enum Tipo Consumidor

--- src/com/company/ServicoConsumidor.java
+++ src/com/company/ServicoConsumidor.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class ServicoConsumidor {
+
+    //Método receber dados para cadastrar
+
+    public static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
+    //Método cadastrar consumidor
+
+    public static Consumidor cadastrarConsumidor(){
+        String nome = capturarDados( "Por favor, digite seu nome: " ).nextLine();
+        String email = capturarDados("Por favor, digite seu email: ").nextLine();
+    }
 }
