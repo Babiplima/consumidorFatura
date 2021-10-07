7b0364cd9007f134960e7a04af6bef2f70106dfd
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-07 16:50:22 -0300

    Criar método captar dados

--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -2,7 +2,19 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sistema {
+
+    //Lista responsável por receber consumidores
+
     private List<Consumidor> consumidor = new ArrayList<>();
+
+    // Método captar dados
+
+    public static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
 }
